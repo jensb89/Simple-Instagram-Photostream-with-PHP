9afdcd910ae38f88cd448402dc67f2185efa09e3
Jens <black1cat@gmail.com>
2012-08-18 12:26:19 +0300

    main file

--- instagram.php
+++ instagram.php
@@ -2,22 +2,19 @@
 $url="https://api.instagram.com/v1/users/self/feed?access_token=TOKEN";
 
 // Get cURL resource
-    $curl = curl_init();
- // Set some options
-    curl_setopt_array($curl, array(
-        CURLOPT_RETURNTRANSFER => 1,
-        CURLOPT_URL => $url
-    ));
+$curl = curl_init();
+ // Options
+curl_setopt_array($curl, array(
+  CURLOPT_RETURNTRANSFER => 1,
+  CURLOPT_URL => $url
+ ));
 // Send the request & save response to $resp
-    $resp = curl_exec($curl);
-// Close request to clear up some resources
-    curl_close($curl);
-    /*if($resp){
-        echo $resp;
-    } //JSON AUSGABE */ 
+$resp = curl_exec($curl);
+curl_close($curl);
 	
-	$data=json_decode($resp, true);
-	foreach($data['data'] as $images){
+$data=json_decode($resp, true);
+
+foreach($data['data'] as $images){
 	echo '<a href="'.$images['link'].'" ><img src="'.$images['images']['thumbnail']['url'].'" /></a>';
-	}
+}
 ?>
