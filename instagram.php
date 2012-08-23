d85a913cb882cc8dd90040229ff6eb0438023ae9
Jens <black1cat@gmail.com>
2012-08-23 20:12:48 +0300

    Check for cURL Error, and set cURL Timeout

--- instagram.php
+++ instagram.php
@@ -6,15 +6,18 @@ $curl = curl_init();
 // Options
 curl_setopt_array($curl, array(
   CURLOPT_RETURNTRANSFER => 1,
-  CURLOPT_URL => $url
+  CURLOPT_URL => $url,
+  CURLOPT_TIMEOUT => 4
  ));
 // Send the request & save response to $resp
 $resp = curl_exec($curl);
 curl_close($curl);
 	
-$data=json_decode($resp, true);
-
-foreach($data['data'] as $images){
-	echo '<a href="'.$images['link'].'" ><img src="'.$images['images']['thumbnail']['url'].'" /></a>';
+if($resp){
+	$data=json_decode($resp, true);
+	foreach($data['data'] as $images){
+	  echo '<a href="'.$images['link'].'" ><img src="'.$images['images']['thumbnail']['url'].'" /></a>';
+	}
 }
+else echo "FEHLER";
 ?>
